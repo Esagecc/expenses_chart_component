42833297d741d3b33b257191ea12e18a94ef644e
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-08-17 16:20:59 -0500

    updated the chart

--- script.js
+++ script.js
@@ -1,25 +1,76 @@
 let xmlhttp = new XMLHttpRequest();
-let url = 
+let url = "http://127.0.0.1:5500/Data.json"
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+        var data = JSON.parse(this.responseText);
+        // console.log(data)
+        day = data.map(function(elem){
+            return elem.day;
+        })
+        amount = data.map(function(elem){
+            return elem.amount;
+        })
+        // console.log(amount)
 
-
-const ctx = document.getElementById('canvas').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        const ctx = document.getElementById('canvas').getContext('2d');
+        const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: day,
         datasets: [{
-    
-            data: [12, 19, 3, 5, 2, 3],
+            data: amount,
             backgroundColor: [
+                'hsl(10, 79%, 65%)',
+                'hsl(10, 79%, 65%)',
+                'hsl(186, 34%, 60%)',
+                'hsl(10, 79%, 65%)',
+                'hsl(10, 79%, 65%)',
+                'hsl(10, 79%, 65%)',
                 'hsl(10, 79%, 65%)'
-            ]
+            ],
+            borderRadius: '3'
         }]
-    },
-    options: {
+        },
+        options: {
+            layout: {
+                padding: {
+                    left: -10,
+                    right: -10
+                }
+            },
         scales: {
             y: {
                 beginAtZero: true
+            },
+            x: {
+                grid: {
+                   display: false,
+                   drawBorder: false
+                },
+                angleLines: {
+                    display: false
+                  }
+             },
+             y: {
+                grid: {
+                   display: false,
+                   drawBorder: false
+                },
+                ticks: {
+                    display: false
+                }
+             },
+        },
+        plugins: {
+            legend: {
+                display: false
             }
         }
     }
 });
+
+    }
+}
+
